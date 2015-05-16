4a5375129f5a1a87de28417a584f6fd82448abbe
Toraja <Toraja@users.noreply.github.com>
2015-05-17 00:15:01 +0900

    testing initShips

--- CH5.1/Ship.java
+++ CH5.1/Ship.java
@@ -1,16 +1,19 @@
+//					completed
+//getHpOfSize		yes
+//Ship				yes
+//getName			yes
+//getHp				yes
+//getSize			yes
+//initShips			
+//getRandomShipName	yes
+
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Ship{
 	private String name;
 	private int hp;
 	private ShipSize size;
-	private static ArrayList<String> shipNameList; // TODO static initializer
-	
-	public Ship(ShipSize size){
-		this.size = size;
-		this.name = getRandomShipName();
-		this.hp = size.getHpOfSize();
-	}
 	
 	public static enum ShipSize{
 		Large(4), Mid(3), Small(2);
@@ -26,10 +29,48 @@ public class Ship{
 		}
 	}
 	
-	public String getRandomShipName(){
-		// TODO implement
+	private Ship(ShipSize size, ArrayList<String> shipNameList){
+		this.size = size;
+		this.name = getRandomShipName(shipNameList);
+		this.hp = size.getHpOfSize();
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+
+	public int getHp(){
+		return this.hp;
+	}
+
+	public ShipSize getSize(){
+		return this.size;
+	}
+	
+	public static ArrayList<Ship> initShips(int totalShipNum){
+		ArrayList<String> shipNameList = new ArrayList<String>(Arrays.asList("Google.com", "Facebood.com", "Instagram.com", "Spotify.com", "Amazon.com", "YouTube.com", "Twitter.com", "Kickstarter.com", "Evernote.com")); 
+		ArrayList<Ship> shipList = new ArrayList<Ship>();
+		int createdShipNum = 0;
+		int shipNumForSize = 0;
 		
-		return "";
+		for(ShipSize shipSize : ShipSize.values()){
+			shipNumForSize = SinkADotCom.getRandomIntUpTo(totalShipNum - createdShipNum);
+			
+			for(int i = 0; i < shipNumForSize; i++){
+				shipList.add(new Ship(shipSize, shipNameList));
+			}
+		}
+		
+		
+		return null;
+	}
+	
+	private static String getRandomShipName(ArrayList<String> shipNameList){
+		int index = SinkADotCom.getRandomIntUpTo(shipNameList.size() - 1);
+		String shipName = shipNameList.get(index);
+		shipNameList.remove(index);
+
+		return shipName;
 	}
 	
 }
---
+++ CH5.1/TestShip.java
@@ -0,0 +1,94 @@
+//					test drive	result
+//getHpOfSize		ok			ok
+//Ship				n/a			n/a
+//getName			n/a			n/a
+//getHp				n/a			n/a
+//getSize			n/a			n/a
+//initShips			
+//getRandomShipName	ok			ok
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.lang.reflect.Method;
+//import Ship;
+
+public class TestShip{
+
+	static Class<?> testClass = Ship.class;
+	static String testFailedMsg = "Case %s failed. %s\n";
+	
+	public static void main(String[] args) {
+//		testGetRandomShipName();
+		testGetHpOfSize();
+	}
+	
+	private static void testGetRandomShipName(){
+		System.out.println("### Start testGetRandomShipName ###");
+		
+		
+		ArrayList<String> shipNameList = new ArrayList<String>(Arrays.asList("Google.com", "facebood.com", "Apple.com", "Microsoft.com", "Wikipedia.com", "YouTube.com", "Twitter.com", "Kickstarter.com", "Evernote.com"));
+
+		try {
+			Method testMethod = testClass.getDeclaredMethod("getRandomShipName", ArrayList.class);
+			testMethod.setAccessible(true);
+			for(int i = 0; i < 3; i++){
+				String shipName = (String)testMethod.invoke(testClass, shipNameList);
+				System.out.println(shipName);
+				System.out.println(Arrays.toString(shipNameList.toArray()));
+				if (shipNameList.contains(shipName)) { // 
+					System.out.format(testFailedMsg, i+1, "");
+				}
+			}
+		} 
+		catch (Exception e) { 
+			e.printStackTrace();
+		}
+
+		System.out.println("### End testGetRandomShipName ###");
+	}
+	
+	private static void testGetHpOfSize(){
+		try {
+			Class<?> shipSize = Ship.ShipSize.class;
+			Method testMethod = shipSize.getDeclaredMethod("getHpOfSize");
+			testMethod.setAccessible(true);
+			
+			System.out.println(testMethod.invoke(Ship.ShipSize.Large));
+			System.out.println(testMethod.invoke(Ship.ShipSize.Mid));
+			System.out.println(testMethod.invoke(Ship.ShipSize.Small));
+		} 
+		catch (Exception e) { 
+			e.printStackTrace();
+		}
+	}
+	
+	private static void testInitShips(){
+		System.out.println("### Start testInitShips ###");
+		
+		int[] input = {5, 7, 9};
+		ArrayList<Ship> shipList = new ArrayList<Ship>();
+		int largeShipNum = 0;		
+		int midShipNum = 0;		
+		int smallShipNum = 0;
+				
+		for(int i = 0; i < input.length; i++){
+			shipList = Ship.initShips(input[i]);
+			
+			System.out.println("total number of ship: " + shipList.size());
+			
+			for(Ship ship : shipList){
+				switch(ship.getSize()){
+					case Ship.ShipSize.Large: largeShipNum++; break;
+					case Ship.ShipSize.Mid: midShipNum++; break;
+					case Ship.ShipSize.Small: smallShipNum++; break;
+				}
+			
+			System.out.println("the number of large ship" + largeShipNum);
+			System.out.println("the number of mid ship" + midShipNum);
+			System.out.println("the number of small ship" + smallShipNum);
+			}
+		
+			System.out.println("### End testInitShips ###");
+		}
+	}
+}
