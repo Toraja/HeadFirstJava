93b846febba77903ac80d052c491a6f888a0eb48
Toraja <Toraja@users.noreply.github.com>
2015-05-27 08:22:11 +0900

    testing checkOnTheEdge

--- CH5.1/TestPlayField.java
+++ CH5.1/TestPlayField.java
@@ -26,7 +26,8 @@ import java.lang.reflect.Field;
 	public static void main(String[] args){
 		System.out.println("### Start TestPlayField ###");
 		
-		testGetRandomLocNum();
+//		testGetRandomLocNum();
+		testCheckOnTheEdge();
 
 		System.out.println("### End TestPlayField ###");
 	}
@@ -89,7 +90,43 @@ import java.lang.reflect.Field;
 	}
 
 	private static void testCheckOnTheEdge(){
+		System.out.println("### Start testCheckOnTheEdge ###");
+		
+		
+		try{
+			StubPlayField spf = new StubPlayField();
+			spf.setFieldLengthAndWidth(8, 8);
+			PlayField playField = spf;
+			
+			Method testMethod = testClass.getDeclaredMethod("checkOnTheEdge");
+			testMethod.setAccessible(true);
 
+			int[] testInput1 = {1, 8, 57, 64}; // every corner 
+			Direction[] UR = {Direction.Up, Direction.Right};
+			Direction[] UL = {Direction.Up, Direction.Left};
+			Direction[] DR = {Direction.Down, Direction.Right};
+			Direction[] DL = {Direction.Down, Direction.Left};
+			Direction[][] expectedValue1 = {DR, DL, UR, UL};
+			boolean testSucceeded = true;
+
+			// test for each corner for each size
+			for(int i = 0; i < ShipSize.values().length; i++){
+				for(int j = 0; j < testInput1.length; j++){
+					ArrayList<Direction> actualResult = testMethod.invoke(playField, testInput[j], ShipSize.values()[i]);
+					if(! Arrays.equals(expectedValue1[j], actualResult.toArray()){
+						System.out.format(testFailedMsg, "corner " + ShipSize.values()[i] + " " + j, "");
+						testSucceeded = false;
+					}
+				}
+			}
+			if(testSucceeded){
+				System.out.println("test for corner succeeded");
+			}
+
+			// test for upper edge
+			testSucceeded = true;
+
+		System.out.println("### End testCheckOnTheEdge ###");
 	}
 
 	private static void testCheckShipOnTheWay(){
