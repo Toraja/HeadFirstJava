26417962e01a54577e9617d0e2b55451b1a83250
Toraja <toraja.blend.french@gmail.com>
2016-07-26 08:20:32 +0900

    some test

---
+++ CH0/MainMethodNamedAsSub.java
@@ -0,0 +1,10 @@
+/**
+ * This will fail
+ * It has to be "main" method
+ */
+public class MainMethodNamedAsSub {
+	public static void sub(String[] args){
+		System.out.print("method 'sub' was called.");
+		//System.out.print('method "sub" was called.');
+	}
+}
